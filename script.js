63a06b6d9e1aae1792d00dde638dcfe57ba636cf
mullenslucas <mullenlucas@gmail.com>
2022-07-19 13:15:22 -0300

    add js script to hamburger icon

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const menuBar = document.querySelector('.menu_bar');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menuBar.classList.toggle('active');
+});
