201ee47eff842747efd0fa3cde51aee70b32a939
RodChak <97179349+RodChak@users.noreply.github.com>
2022-07-19 20:25:14 +0200

    fix linter errors
    Signed-off-by: RodChak <97179349+RodChak@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-const hamburger = document.querySelector('.hamburger');
-const menuBar = document.querySelector('.menu_bar');
+  const hamburger = document.querySelector('.hamburger');
+  const menuBar = document.querySelector('.menu_bar');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  menuBar.classList.toggle('active');
-});
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    menuBar.classList.toggle('active');
+  });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  menuBar.classList.remove('active');
-}));
+  document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menuBar.classList.remove('active');
+  }));
 });
